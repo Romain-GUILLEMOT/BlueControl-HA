3176700158a4a27a4aab418cde678b1c0ddf4580
romainguillemot <contact@romain-guillemot.dev>
2024-12-22 20:53:37 +0100

    Initial Commit

---
+++ BlueControl HA/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BlueControl HA
+//
+//  Created by Romain Guillemot on 22/12/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ BlueControl HATests/BlueControl_HATests.swift
@@ -0,0 +1,17 @@
+//
+//  BlueControl_HATests.swift
+//  BlueControl HATests
+//
+//  Created by Romain Guillemot on 22/12/2024.
+//
+
+import Testing
+@testable import BlueControl_HA
+
+struct BlueControl_HATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
